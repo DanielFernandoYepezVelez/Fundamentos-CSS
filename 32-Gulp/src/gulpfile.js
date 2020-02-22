39133084a2d8458391254305450bb25bfefd681f
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2020-02-22 17:55:18 -0500

    Agregando Documentacion de gulp con pug y sass

--- 32-Gulp/src/gulpfile.js
+++ 32-Gulp/src/gulpfile.js
@@ -1,13 +1,38 @@
 const gulp = require('gulp');
+const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('tarea 1', () => {
-    console.log('Hola Amigo yo soy la tarea UNO');
-    console.log('Hola Amigo yo soy la tarea DOS');
+// gulp.task('default', () => {
+//     console.log('Hola Amigo yo soy la tarea UNO DEFAULT');
+//     console.log('Hola Amigo yo soy la tarea DOS DEFAULT');
+// });
+
+// gulp.task('tarea 1', () => {
+//     console.log('Hola Amigo yo soy la tarea UNO');
+//     console.log('Hola Amigo yo soy la tarea DOS');
+// });
+
+gulp.task('sass', () => {
+    gulp.src('./dev/scss/*.scss')
+        .pipe(sass({
+            outputStyle: 'nested'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowsersList: ['last 3 versions']
+        }))
+        .pipe(gulp.dest('./public/css/'))
+});
+
+gulp.task('pug', () => {
+    gulp.src('./dev/pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/'))
 });
 
 gulp.task('default', () => {
-    console.log('Hola Amigo yo soy la tarea UNO DEFAULT');
-    console.log('Hola Amigo yo soy la tarea DOS DEFAULT');
+    gulp.watch('./dev/scss/*.scss', gulp.series('sass'))
+    gulp.watch('./dev/pug/*.pug', gulp.series('pug'))
 });
